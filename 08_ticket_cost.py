bc73eba367f6353e0bd01cb3bc205f24cdc512ba
charlizeglanville <24548@tgs.school.nz>
2024-06-20 12:56:46 +1200

    This is the updated version!

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,4 +1,15 @@
+# Next steps
+# Add currency formatting & Headings
+# Intergrate this componenet with the bas component
+
+# PS: In Base component remember to calculate surcharge
+# Once payment method has been chosen!!
+
 import pandas
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
 
 # dictionaries to hold ticket details
 all_names =["a", "b", "c", "d", "e"]
@@ -12,6 +23,7 @@ mini_movie_dict = {
 }
 
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
 # Calculate the total ticket cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
@@ -25,9 +37,20 @@ mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
 
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print("------ Ticket Data ------")
+print()
+
 # output table with ticket data
 print(mini_movie_frame)
 
-# output total ticket sales and profit 
+print()
+print("----- Ticket Cost / Profit -----")
+
+# output total ticket sales and profit
 print("Total Ticket Sales: ${:.2f}".format(total))
 print("Total Profit: ${:.2f}".format(profit))
